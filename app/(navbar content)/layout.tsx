4c01abba2f8f0a0dc1b2e44a32b96973e59ded49
ujala <singhujala2004@gmail.com>
2024-10-01 20:36:54 +0530

    Refactor code to add footer component

--- app/(navbar content)/layout.tsx
+++ app/(navbar content)/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
                 </div>
             </div>
             {children}
+            <footer className="w-full  text-white border-t-2 border-white py-6 flex justify-center">
+                <p className="w-[80rem] tracking-wide">&copy; Copy right 2024 @Ujala Singh , All rights reserved</p>
+            </footer>
         </main>
     );
 }
