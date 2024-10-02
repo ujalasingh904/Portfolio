3f08d97c4ebc90e9f7b289aa67c368953ab18fe9
ujala <singhujala2004@gmail.com>
2024-10-02 21:12:14 +0530

    Refactor layout.tsx to update padding in the navbar content

--- app/(navbar content)/layout.tsx
+++ app/(navbar content)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
     return (
         <main>
             <div data-aos="fade-down" className="w-full flex justify-center fixed top-0 p-4 md:p-6 z-[1000] text-white">
-                <div className="max-w-[20rem] sm:max-w-[30rem] mx-auto flex justify-center  border-4 rounded-full p-5 px-7 shadow-3xl backdrop-blur-xl">
+                <div className="max-w-[20rem] sm:max-w-[30rem] mx-auto flex justify-center  border-4 rounded-full p-3 md:p-5 px-7 shadow-3xl backdrop-blur-xl">
                     <ul className="flex space-x-6">
                         <li className="relative group new">
                             <Link href="/">Home</Link>
