ca1253543d5cb4cf1151640e15e7eaf085ba147f
ujala <singhujala2004@gmail.com>
2025-01-24 13:29:49 +0530

    Update footer copyright year to 2025

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full  text-white border-t-2 border-white p-4 py-3 md:py-6 flex justify-center">
-            <p className="w-full md:max-w-5xl lg:max-w-6xl xl:max-w-7xl tracking-wide text-sm md:text-base">&copy; Copy right 2024 @Ujala Singh , All rights reserved</p>
+            <p className="w-full md:max-w-5xl lg:max-w-6xl xl:max-w-7xl tracking-wide text-sm md:text-base">&copy; Copy right 2025 @Ujala Singh , All rights reserved</p>
         </footer>
     );
 }
